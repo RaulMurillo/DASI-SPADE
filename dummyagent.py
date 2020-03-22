be8ab4c585de8247eb306209c354c26200ef327f
RaulMurillo <ramurimontero@gmail.com>
2020-03-22 20:39:31 +0100

    Added sample Agent

---
+++ dummyagent.py
@@ -0,0 +1,35 @@
+import time
+import asyncio
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+
+class DummyAgent(Agent):
+    class MyBehav(CyclicBehaviour):
+        async def on_start(self):
+            print("Starting behaviour . . .")
+            self.counter = 0
+
+        async def run(self):
+            print("Counter: {}".format(self.counter))
+            self.counter += 1
+            await asyncio.sleep(1)
+
+    async def setup(self):
+        print("Agent starting . . .")
+        b = self.MyBehav()
+        self.add_behaviour(b)
+
+if __name__ == "__main__":
+    dummy = DummyAgent("dummy01@404.city", "user01")
+
+    future = dummy.start()
+    future.result()  # Wait until the start method is finished
+
+    print("Wait until user interrupts with ctrl+C")
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+    dummy.stop()
+    print("Agent stopped!")
